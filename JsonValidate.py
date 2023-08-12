acb9ef5a9970204d14699bcb21466e23b9a4b295
bmiit145 <21bmiit145@gmail.com>
2023-08-12 22:51:04 +0530

    make aPI

--- JsonValidate.py
+++ JsonValidate.py
@@ -1,4 +1,7 @@
+from flask import Flask, request, jsonify
 import json
+
+app = Flask(__name__)
 
 def validate_json(json_data):
     try:
@@ -18,3 +21,12 @@ json_data = '[{"abc":"1","name":"ascf"},{"abc":"6","name":"65"}]'
 
 result = validate_json(json_data)
 print(result)
+
+@app.route('/validate', methods=['POST'])
+def validate_endpoint():
+    json_data = request.data.decode('utf-8')
+    result = validate_json(json_data)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
